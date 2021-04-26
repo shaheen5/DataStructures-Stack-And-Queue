ca9af7eef778ddc991cdaa5952dcd52d94a2dd84
shaheen5 <miyashaheen1995@gmail.com>
2021-04-26 07:11:37 +0530

    Added : Code To Display Message

---
+++ src/main/java/com/datastructures/MyStack.java
@@ -0,0 +1,7 @@
+package com.datastructures;
+
+public class MyStack {
+    public static void main(String[] args) {
+        System.out.println("Data Structures - Stack And Queue - Problem Using LinkedList");
+    }
+}
